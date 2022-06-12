a251309cb1db3275660842b4b0fede079f35594f
yjiu99 <yjiu9992@gmail.com>
2022-06-13 07:47:53 +0900

    추가

---
+++ api/urls.py
@@ -0,0 +1,7 @@
+from api.views import TodosAPIView,TodoDetailAPIView
+from django.urls import path,include
+
+urlpatterns = [
+    path("", TodosAPIView.as_view(),name="todos"),
+    path('<int:id>',TodoDetailAPIView.as_view(),name='todo'),
+]
